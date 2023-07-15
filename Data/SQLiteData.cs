4a453c0fc234f199532d11c4d4dee19f3ca9d8c9
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-07-15 16:46:39 -0300

    tabela de usuario

---
+++ Data/SQLiteData.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using Tela_Login.Model;
+
+namespace Tela_Login.Data
+{
+    public class SQLiteData
+    {
+        readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteData(string path)
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Usuario>().Wait();
+        }
+        //public UsuarioData UsusarioDatable { get; set; }
+
+    }
+}
---
+++ Model/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Tela_Login.Model
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
